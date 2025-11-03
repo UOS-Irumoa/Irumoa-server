89f9c11bd7c105c21faa5b01823e5dee4e894493
ddoong10 <ddoong10@naver.com>
2025-11-03 18:31:44 +0900

    spring boot init

---
+++ irumoa/src/main/java/uos/pbl/irumoa/IrumoaApplication.java
@@ -0,0 +1,13 @@
+package uos.pbl.irumoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrumoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrumoaApplication.class, args);
+	}
+
+}
---
+++ irumoa/src/test/java/uos/pbl/irumoa/IrumoaApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.pbl.irumoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrumoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
